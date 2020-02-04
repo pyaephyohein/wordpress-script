3eab5e1ae5e950c0d2660f4d75a53ff0cf4ad4a7
Pyae Phyo Hein <pyaephyohein@mail@pyaephyohein.com>
2020-02-04 16:54:37 +0630

    second

--- wordpress.sh
+++ wordpress.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+# Wordpress install in LAMP
 echo "Much run with root!"
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install -y  apache2
 sudo apt install -y php*
 sudo apt install -y mysql*
+sudo mkdir /root/wordpressdownload
+cd /root/wordpressdownload
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xzf latest.tar.gz
+rm -rf /var/www/html/index.html
+cp wordpress/* /var/www/html/
 
