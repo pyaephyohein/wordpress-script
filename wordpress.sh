2fc085c08d2868aa0d226c233fb9f3ea66b27f26
Pyae Phyo Hein <pyaephyohein@mail@pyaephyohein.com>
2020-02-04 17:35:16 +0630

    3rd

--- wordpress.sh
+++ wordpress.sh
@@ -3,13 +3,14 @@
 echo "Much run with root!"
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y  apache2
-sudo apt install -y php*
-sudo apt install -y mysql*
+sudo apt install -y apache2
+sudo apt install -y php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+sudo apt install -y mysql-server
+sudo mysql_secure_installation
 sudo mkdir /root/wordpressdownload
 cd /root/wordpressdownload
 sudo wget https://wordpress.org/latest.tar.gz
 sudo tar -xzf latest.tar.gz
-rm -rf /var/www/html/index.html
-cp wordpress/* /var/www/html/
+rm -rf /var/www/html/*
+cp -r wordpress/* /var/www/html/
 
